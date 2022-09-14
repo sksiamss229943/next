6cb7f5d6b90fcfb83bed35c0bd6d557fdf52c92a
sksiamss229943@gmail.com <sksiamss229943@gmail.com>
2022-09-15 00:01:50 +0600

    Simple blog website

---
+++ pages/posts/[id].js
@@ -0,0 +1,36 @@
+import axios from "axios";
+function Post({ post }) {
+  return (
+    <>
+      <div
+        className="post"
+        style={{
+          cursor: "pointer",
+          userSelect: "none",
+          background: "orange",
+          margin: "20px",
+          borderRadius: "10px",
+          padding: "10px",
+          color: "white",
+        }}
+      >
+        <h2>{post.title}</h2>
+        <h5>{post.body}</h5>
+      </div>
+    </>
+  );
+}
+
+export const getServerSideProps = async (ctx) => {
+  const { params } = ctx;
+  const post = (
+    await axios.get(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+  ).data;
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export default Post;
---
+++ pages/posts/index.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import Link from "next/link";
+
+const posts = ({ posts }) => {
+  return (
+    <>
+      {posts.map((post) => (
+        <>
+          <Link href={`/posts/${post.id}`} passHref>
+            <div
+              className="post"
+              style={{
+                cursor: "pointer",
+                userSelect: "none",
+                background: "orange",
+                margin: "20px",
+                borderRadius: "10px",
+                padding: "10px",
+                color: "white",
+              }}
+            >
+              <h2>{post.title}</h2>
+              <h5>{post.body}</h5>
+            </div>
+          </Link>
+        </>
+      ))}
+    </>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const posts = (await axios.get("https://jsonplaceholder.typicode.com/posts"))
+    .data;
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default posts;
