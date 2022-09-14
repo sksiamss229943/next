61044ad5ce756020532c50adb84627ccdd57c523
sksiamss229943@gmail.com <sksiamss229943@gmail.com>
2022-09-14 23:19:24 +0600

    API updating test

---
+++ components/user.js
@@ -0,0 +1,11 @@
+const User = ({ name, email }) => {
+  return (
+    <>
+      <h2>Name: {name}</h2>
+      <h4>Email: {email}</h4>
+      <br />
+    </>
+  );
+};
+
+export default User;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Ohiduzzaman - Siam</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
