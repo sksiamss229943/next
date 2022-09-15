34d7695dfdf81c058dbd254c19deec31adca1294
sksiamss229943@gmail.com <sksiamss229943@gmail.com>
2022-09-15 10:37:51 +0600

    Padding and margin updating

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -4,35 +4,38 @@ import Link from "next/link";
 const posts = ({ posts }) => {
   return (
     <>
-      <h1
-        style={{
-          textAlign: "center",
-          margin: "20px 0",
-          fontSize: "2.7rem",
-        }}
-      >
-        Siam Blogs
-      </h1>
-      {posts.map((post) => (
-        <>
-          <Link href={`/posts/${post.id}`}>
-            <div
-              className="post"
-              style={{
-                cursor: "pointer",
-                userSelect: "none",
-                background: "orange",
-                margin: "0 20px 20px 20px",
-                borderRadius: "5px",
-                padding: "10px",
-                color: "white",
-              }}
-            >
-              <h2>{post.title}</h2>
-            </div>
-          </Link>
-        </>
-      ))}
+      <div className="container" style={{padding: "0 20px"}}>
+        <h1
+          style={{
+            textAlign: "center",
+            margin: "20px 0",
+            fontSize: "2.7rem",
+          }}
+        >
+          Siam Blogs
+        </h1>
+        {posts.map((post) => (
+          <>
+            <Link href={`/posts/${post.id}`}>
+              <div
+                className="post"
+                style={{
+                  cursor: "pointer",
+                  userSelect: "none",
+                  background: "orange",
+                  margin: "0 auto 20px",
+                  borderRadius: "5px",
+                  padding: "10px",
+                  color: "white",
+                  maxWidth: "50rem",
+                }}
+              >
+                <h2>{post.title}</h2>
+              </div>
+            </Link>
+          </>
+        ))}
+      </div>
     </>
   );
 };
